c37b3c033be6b16b1c8d57ba6d4b7d2c03d6bd60
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-04-29 16:28:55 +0000

    menuscene

--- js/menuScene.js
+++ js/menuScene.js
@@ -15,7 +15,6 @@ class MenuScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "menuScene" });
-    console.log("Menu Scene");
   }
 
   /**
@@ -33,7 +32,7 @@ class MenuScene extends Phaser.Scene {
    * Use it to load assets.
    */
   preload() {
-
+    console.log("Menu Scene");
   }
 
   /**
