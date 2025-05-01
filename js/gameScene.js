a46ee7da7a9f1235e4c0ed536b620f548639f788
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-01 16:19:19 +0000

    sync check

--- js/gameScene.js
+++ js/gameScene.js
@@ -4,20 +4,17 @@
 //
 // Created by: Isaac Ip
 // Created on: Apr 2025
-// This is the Menu Scene
+// This is the Game Scene
 
 /**
- * This class is the Menu Scene.
+ * This class is the Splash Scene.
  */
-class MenuScene extends Phaser.Scene {
+class GameScene extends Phaser.Scene {
   /**
    * This method is the constructor.
    */
   constructor() {
-    super({ key: "menuScene" });
-
-    this.MenuSceneBackgroundImage = null;
-    this.startButton = null;
+    super({ key: "gameScene" });
   }
 
   /**
@@ -35,9 +32,7 @@ class MenuScene extends Phaser.Scene {
    * Use it to load assets.
    */
   preload() {
-    console.log("Menu Scene");
-    this.load.image("menuSceneBackground", "./assets/aliens_screen_image2.jpg");
-    this.load.image("startButton", "./assets/start.png");
+    console.log("Game Scene");
   }
 
   /**
@@ -46,28 +41,18 @@ class MenuScene extends Phaser.Scene {
    * @param {object} data Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    this.MenuSceneBackgroundImage = this.add.sprite(0, 0, "menuSceneBackground");
-    this.MenuSceneBackgroundImage.x = 1920 / 2;
-    this.MenuSceneBackgroundImage.y = 1080 / 2; 
-
-    this.startButton = this.add.sprite(1920 / 2, 1080 / 2 + 100, "startButton");
-    this.startButton.setInteractive({useHandCursor: true});
-    this.startButton.on("pointerdown", () => this.clickButton());
-    };
+    // pass
   }
 
   /**
    * Should be overridden by your own Scenes.
    * This method is called once per game step while the scene is running.
-   * @param {number} time The current time.
-   * @param {number} delta The delta time in ms since the last frame.
+   * @param {number} time - The current time.
+   * @param {number} delta - The delta time in ms since the last frame.
    */
   update(time, delta) {
     // pass
   }
+}
 
-  clickButton() {
-    this.scene.start("gameScene");
-  }
-
-export default MenuScene
+export default GameScene
